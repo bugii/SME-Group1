4aa83d4744d06137278d3a0b3e3a808c05ee14bf
chfaes <christoph.faessler@uzh.ch>
2019-10-15 12:29:54 +0200

    Work in progress - python webscraping main

--- Python/Web Scraping/main.py
+++ Python/Web Scraping/main.py
@@ -7,8 +7,10 @@ from bs4 import BeautifulSoup
 # try pip install bs4 and pip install requests.
 # All packages via py -m pip freeze are in the Requirements.txt in the main folder; install via pip install -m Requirements.txt
 
-#url = 'https://de.wikipedia.org/wiki/Wikipedia:Hauptseite'
-url = 'https://mvnrepository.com/artifact/org.apache.commons'
+#
+# This code scans the complete apache maven repository and outputs all artifacts found including their dependencies
+#
+url = 'https://mvnrepository.com/artifact/org.apache'
 response = requests.get(url)
 #sleep is here just to get used to it early on: Don't spam servers, you run the risk of being blocked
 time.sleep(0.1)
@@ -16,25 +18,54 @@ print(response)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 soup.findAll('a')
-#print(soup)
 
-
+Linklist = []
 for link in soup.find_all('a'):
-    #find all links that do not end on "usages" (otherwise all links would be de facto duplicated)
-    if(link.get('href').startswith('/artifact/org.apache.commons') and not link.get('href').endswith('usages')):
-        print(link.get('href'))
+    Linklist.append(link.get('href'))
+print("links in " + url + " : ")
+print(Linklist)
 
+Linklist2 = []
+for x in Linklist:
+    if  x.startswith("/artifact"):
+        Linklist2.append(x)
+print("filtered links:")
+print(Linklist2)
 
-"""
-lists = soup.select("")
-print(lists)
-for x in lists:
-    print(x.text)
-string1 = str(soup)
-#print(string1)
-"""
+print(" ")
+print("starting loop")
 
-#one_a_tag = soup.findAll('a')[36]
-#link = one_a_tag['href']
-#download_url = 'https://de.wikipedia.org/wiki/Wikipedia:Hauptseite'+ link
-#urllib.request.urlretrieve(download_url, './' + link[link.find('/turnstile_') + 1:])
+for y in range(len(Linklist2)-1):
+    time.sleep(1)
+    url2 = 'https://mvnrepository.com' + Linklist2[y]
+    print(url2)
+    response = requests.get(url2)
+
+    print(response)
+    Linklist.clear()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    soup.findAll('a')
+    for link in soup.find_all('a'):
+        Linklist.append(link.get('href'))
+    print(Linklist)
+
+    Linklist3=[]
+    for x in Linklist:
+        if x.startswith('/artifact/org.apache.maven') and not x.endswith('/usages'):
+            Linklist3.append(x)
+    print(Linklist3)
+
+    """______________________________________________
+    lists = soup.select("")
+    print(lists)
+    for x in lists:
+        print(x.text)
+    string1 = str(soup)
+    #print(string1)
+   
+
+    #one_a_tag = soup.findAll('a')[36]
+    #link = one_a_tag['href']
+    #download_url = 'https://de.wikipedia.org/wiki/Wikipedia:Hauptseite'+ link
+    #urllib.request.urlretrieve(download_url, './' + link[link.find('/turnstile_') + 1:])
+     """
