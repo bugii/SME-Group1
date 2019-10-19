54e21e69aa2c28137a40e5899e1bdca53d6ebbe8
chfaes <christoph.faessler@uzh.ch>
2019-10-19 15:28:08 +0200

    Webscraper finished: can now copy all dependencies from Maven Repo to file

---
+++ Python/Web Scraping/functions.py
@@ -0,0 +1,89 @@
+import time
+import random
+import os
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def wait_random():
+# Generates a random wait time, usually around 1 to 2 seconds, with some longer intervals up to 7 seconds added for
+# good measure.
+    int1 = random.random()
+    print("Pause for a short time...")
+    if(int1>0.8):
+        time.sleep(4+(3*int1))
+    else:
+        time.sleep(0.8)
+        time.sleep(int1*2)
+
+
+def write_to_txt(string1):
+# Writes the string supplied into a txt file in the main directory.
+    f = open("output.txt", "a")
+    f.write(string1+"\n")
+    f.close()
+
+def write_to_dependencies_txt(string1):
+    f = open("dependencies.txt", "a")
+    f.write(string1 + "\n")
+    f.close()
+
+
+def return_all_links(url):
+# As it says: Returns all links from a website.
+    response = requests.get(url)
+    print(response)
+    wait_random()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    soup.findAll('a')
+    Linklist = []
+    for link in soup.find_all('a'):
+        Linklist.append(link.get('href'))
+    return(Linklist)
+
+
+def deepscan_url(url):
+# Note that this is a very specific block of code. It extracts all urls that end on .pom.
+    Linklist = return_all_links(url)
+    for x in Linklist:
+        if x.endswith(".pom"):
+            write_to_txt(url + x)
+            print("Writing to txt: " + url + x)
+        else:
+            if x.endswith("/") and not x.endswith("../"):
+                print("Proceeding to: "+ url + x)
+                deepscan_url(url + x)
+
+def create_dependency_from_str_soup(soup):
+    # The output looks like this: MyProject/MyProject/1.4.2::, which encodes groupId, artifactId and version
+    # of the project; this is followed by dependencies in the exact same format, seperated by a single colon.
+    # I.e.: MyProject/MyProject/1.4.2::jUnit/jUnit/1.2.7:OtherGroup/OtherArtifact/1.0.1
+    outputline = ""
+    outputline = outputline + str(soup[soup.find("<groupid>")+9:soup.find("</groupid>")]) + "/"
+    outputline = outputline + str(soup[soup.find("<artifactid>")+12:soup.find("</artifactid>")]) + "/"
+    outputline = outputline + str(soup[soup.find("<version>")+9:soup.find("</version>")]) + "::"
+    print(outputline)
+
+    if soup.find("<dependencies>") != -1:
+        dependencies = str(soup[soup.find("<dependencies>")+14:soup.find("</dependencies>")]).splitlines()
+        print(dependencies)
+        temp_string = ""
+        for x in dependencies:
+            if x.startswith("<groupid>") or x.startswith("<artifactid>"):
+                temp_string = temp_string + x[x.find(">")+1:x.find("</")] + "/"
+            elif x.startswith("<version>"):
+                outputline = outputline + temp_string + x[x.find(">")+1:x.find("</")] + ":"
+                temp_string = ""
+    else:
+        outputline = outputline + "none"
+
+    return(outputline)
+
+
+def dependencies_from_pom_url(url):
+    response = requests.get(url)
+    print(response)
+    wait_random()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    write_to_dependencies_txt(create_dependency_from_str_soup(str(soup)))
--- Python/Web Scraping/main.py
+++ Python/Web Scraping/main.py
@@ -1,71 +1,27 @@
 import requests
 import urllib.request
-import time
 from bs4 import BeautifulSoup
+import functions
 
-# installed requests and bs4 via py -m pip install bs4 and py -m pip install requests; alternatively, you may
-# try pip install bs4 and pip install requests.
-# All packages via py -m pip freeze are in the Requirements.txt in the main folder; install via pip install -m Requirements.txt
+# I installed the requests library and bs4 library via the following command line input: "py -m pip install bs4" and
+# "py -m pip install requests"; alternatively, you can try "pip install bs4" and "pip install requests".
+# This can also be done at once: All packages (via py -m pip freeze) are in the Requirements.txt in the main folder;
+# they can be installed via "pip install -m Requirements.txt".
 
-#
-# This code scans the complete apache maven repository and outputs all artifacts found including their dependencies
-#
-url = 'https://mvnrepository.com/artifact/org.apache'
-response = requests.get(url)
-#sleep is here just to get used to it early on: Don't spam servers, you run the risk of being blocked
-time.sleep(0.1)
-print(response)
+# Here we scan the complete https://repo.maven.apache.org/maven2/ and copy all the .pom links into a txt in the main
+# directory.
 
-soup = BeautifulSoup(response.text, 'html.parser')
-soup.findAll('a')
+open('output.txt', 'w').close() # Clears the output file at the beginning.
+functions.deepscan_url('https://repo.maven.apache.org/maven2/')
 
-Linklist = []
-for link in soup.find_all('a'):
-    Linklist.append(link.get('href'))
-print("links in " + url + " : ")
-print(Linklist)
+# We now want to copy the contents (all the lines, the urls) from output.txt into a list.
 
-Linklist2 = []
-for x in Linklist:
-    if  x.startswith("/artifact"):
-        Linklist2.append(x)
-print("filtered links:")
-print(Linklist2)
+with open("output.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
 
-print(" ")
-print("starting loop")
+# Next, we create another txt, this time containing the dependencies.
 
-for y in range(len(Linklist2)-1):
-    time.sleep(1)
-    url2 = 'https://mvnrepository.com' + Linklist2[y]
-    print(url2)
-    response = requests.get(url2)
-
-    print(response)
-    Linklist.clear()
-    soup = BeautifulSoup(response.text, 'html.parser')
-    soup.findAll('a')
-    for link in soup.find_all('a'):
-        Linklist.append(link.get('href'))
-    print(Linklist)
-
-    Linklist3=[]
-    for x in Linklist:
-        if x.startswith('/artifact/org.apache') and not x.endswith('/usages'):
-            Linklist3.append(x)
-    print(Linklist3)
-
-    """______________________________________________
-    lists = soup.select("")
-    print(lists)
-    for x in lists:
-        print(x.text)
-    string1 = str(soup)
-    #print(string1)
-   
-
-    #one_a_tag = soup.findAll('a')[36]
-    #link = one_a_tag['href']
-    #download_url = 'https://de.wikipedia.org/wiki/Wikipedia:Hauptseite'+ link
-    #urllib.request.urlretrieve(download_url, './' + link[link.find('/turnstile_') + 1:])
-     """
+open('dependencies.txt', 'w').close() # Clears the dependencies file at the beginning.
+for x in content:
+    functions.dependencies_from_pom_url(x)
