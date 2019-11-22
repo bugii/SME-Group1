d6bfade6e6029efc9e6d3325de152d1aa8630459
Dario Bugmann <dario.bugmann@uzh.ch>
2019-11-22 11:21:45 +0100

    added idea in order to get more information about the projects

--- Python/Docker/project.py
+++ Python/Docker/project.py
@@ -1,11 +1,14 @@
 class Project:
 
-    def __init__(self, name, url, last_updated):
+    def __init__(self, name, url, creation, last_updated, language, contributors ):
         self.name = name
         self.url = url
+        self.creation = creation
         self.last_updated = last_updated
         self.microservices = []
         self.depends_on = 0
+        self.language = language
+        self.contributors = contributors
 
     def __str__(self):
         return self.name
--- Python/Docker/repo-fetching.py
+++ Python/Docker/repo-fetching.py
@@ -24,45 +24,62 @@ session.auth = ('bugii', API_KEY)
 
 def search_github(page):
     print('On page', page)
-    res = session.get('https://api.github.com/search/code',
-                      params={'q': 'path:/ filename:docker-compose.yml',
-                              'sort': 'stars',
-                              'order': 'desc',
-                              'per_page': per_page,
-                              'page': page})
+    '''
+    Do 4 different queries based on different file sizes
+    '''
+    sizes = ["1..1000", "1001..2000", "2001..3000", "3001..4000", "5001..6000", "6001..7000", "7001..8000", "8001..9000", "9001..10000"]
 
-    if res.status_code != 200:
-        print('Something went wrong', res.status_code)
+    for size in sizes:
+        print(size)
 
-    res_json = res.json()
+        res = session.get('https://api.github.com/search/code',
+                          params={'q': 'path:/ filename:docker-compose.yml size:' + size,
+                                  'sort': 'stars',
+                                  'order': 'desc',
+                                  'per_page': per_page,
+                                  'page': page})
 
-    if page == 1:
-        total_repos = res_json['total_count']
-        print('Found:', total_repos)
+        if res.status_code != 200:
+            print('Something went wrong', res.status_code)
 
-    for i in res_json['items']:
-        time.sleep(5)
+        res_json = res.json()
 
-        repo_url = i['repository']['url']
-        print('Repo:', repo_url)
+        if page == 1:
+            total_repos = res_json['total_count']
+            print('Found:', total_repos)
 
-        # get the latest update date
-        repo = session.get(repo_url).json()
-        last_updated_at = datetime.fromisoformat(repo['updated_at'][:-1])
-        name = repo['full_name']
+        for i in res_json['items']:
+            time.sleep(5)
 
-        # create object
-        project = Project(name=name, url=repo_url, last_updated=last_updated_at)
+            repo_url = i['repository']['url']
+            print('Repo:', repo_url)
 
-        # download the repo and unzip it, delete compressed file afterwards
-        archive_path = wget.download(repo_url + '/tarball', 'repositories')
-        folder_name = archive_path[:-7]
-        tarfile.open(archive_path).extractall(folder_name)
-        os.remove(archive_path)
+            # get the creation date of the project
 
-        # save project object inside the folder
-        with open(folder_name + '/project.pkl', 'wb') as f:
-            pickle.dump(project, f, pickle.HIGHEST_PROTOCOL)
+
+            # get the latest update date
+            repo = session.get(repo_url).json()
+            last_updated_at = datetime.fromisoformat(repo['updated_at'][:-1])
+            name = repo['full_name']
+
+            # get the most used language
+
+            # get the contributors
+
+            # if language either python, java, c#, javascript, or go: run the following code
+
+            # # create object
+            # project = Project(name=name, url=repo_url, last_updated=last_updated_at)
+            #
+            # # download the repo and unzip it, delete compressed file afterwards
+            # archive_path = wget.download(repo_url + '/tarball', 'repositories')
+            # folder_name = archive_path[:-7]
+            # tarfile.open(archive_path).extractall(folder_name)
+            # os.remove(archive_path)
+            #
+            # # save project object inside the folder
+            # with open(folder_name + '/project.pkl', 'wb') as f:
+            #     pickle.dump(project, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
